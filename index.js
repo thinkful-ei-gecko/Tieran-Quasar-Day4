4b96d7a77f6cb28654e4444e48f891e3aaaf8f97
Tieran Dysart <tierandysart@gmail.com>
2019-08-15 10:43:36 -0600

    cat carousel

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-function doIt() {
-  const toDos = $('.js-to-dos');
-  // this will grab the first parent of toDos that has the `.js-parent-demo`
-  // class
-  toDos.parent('.js-parent-demo').removeClass('hidden');
-  // this will look at all children of toDos that have the `.js-complete class`
-  toDos.find('.js-complete').addClass('complete');
-}
+$('.thumbnail').on('click', e => {
+  console.log(e.target);
+  const selectedImg = $(e.target); //selectedImg becomes jQuery target on click
+  $('.hero img').attr('src', selectedImg.attr('src'));//jQuery calls class hero and selecting img 
+  //attr taking the current src image and changing it value with new img that is clicked
+  $('.hero img').attr('alt', selectedImg.attr('alt'));//changes the alt tag to the image selected alt tag
+});
 
-doIt();
