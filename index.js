f3a60a3ef85e0db926bc0e386a00f117a442ef21
Quasar Wei <quasar.wei@outlook.com>
2019-08-15 08:55:32 -0700

    went over 2nd example

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function doHelloWorld() {
-    $('.js-hide-it').addClass('hidden');
-    $('.js-hello-world').text('hello world from JS');
-  }
-  
-  doHelloWorld();
+'use strict';
+
+function doIt() {
+  const toDos = $('.js-to-dos');
+  // this will grab the first parent of toDos that has the `.js-parent-demo`
+  // class
+  toDos.parent('.js-parent-demo').removeClass('hidden');
+  // this will look at all children of toDos that have the `.js-complete class`
+  toDos.find('.js-complete').addClass('complete');
+}
+
+doIt();
